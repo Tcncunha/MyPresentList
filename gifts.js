9e20bb7d88e9c9ccd73b725b924cf676bbc9f0c5
TcnCunha <64099736+Tcncunha@users.noreply.github.com>
2025-11-02 20:17:59 +0100

    Add files via upload

---
+++ gifts.js
@@ -0,0 +1,93 @@
+// Gift list data
+const GIFTS_DATA = [
+  {
+    "name": "Polo Shirt",
+    "description": "A nice polo shirt for casual occasions",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Steam Gift Card",
+    "description": "For buying games on Steam",
+    "price": "",
+    "link": "https://store.steampowered.com/digitalgiftcards/"
+  },
+  {
+    "name": "Lego One Piece (Original)",
+    "description": "Official Lego One Piece set",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Electric Scooter",
+    "description": "For easy commuting around the city",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "BBQ Kit",
+    "description": "Complete set for barbecue",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Sweatpants",
+    "description": "Comfortable pants for lounging",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Non-Electric Scooter",
+    "description": "Manual kick scooter",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "3D Hologram Photo Frame (Geek Magic)",
+    "description": "Cool 3D holographic display frame",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "T-shirts",
+    "description": "Casual t-shirts",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Social Socks",
+    "description": "Fun and stylish socks",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Funko Pop Luffy Gear 5",
+    "description": "One Piece collectible figure",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Gurren Lagann Framed Poster",
+    "description": "Poster with quotes 'I Drill to Heaven' or 'Who da Hell do think I'm?'",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Tournament Arc: Build Your Own Sports Anime with Friends!",
+    "description": "Fun board/card game about creating sports anime",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Kamen Rider Item",
+    "description": "Any Kamen Rider collectible or merchandise",
+    "price": "",
+    "link": ""
+  },
+  {
+    "name": "Socks",
+    "description": "Regular comfortable socks",
+    "price": "",
+    "link": ""
+  }
+];
